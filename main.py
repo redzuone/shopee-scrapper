60a35e0c461dda93bfa98916a40438e0e4b44acb
redzuone <21290781+redzuone@users.noreply.github.com>
2021-12-03 22:05:17 +0800

    improve readability

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ from bs4 import BeautifulSoup
 def get_openai_response(review_obj):
     response = openai.Completion.create(
         engine="curie",
-        prompt="This is a Sentence sentiment classifier\n\n\
-            Sentence: \"I loved the new Batman movie!\"\
+        prompt='This is a Sentence sentiment classifier\n\n\
+            Sentence: "I loved the new Batman movie!"\
             \nSentiment: Positive\n\
             ###\n\
-            Sentence: \"I hate it when my phone battery dies.\"\n\
+            Sentence: "I hate it when my phone battery dies."\n\
             Sentiment: Negative\n\
             ###\n\
-            Sentence: \"This is the link to the article\"\n\
+            Sentence: "This is the link to the article"\n\
             Sentiment: Neutral\n\
             ###\n\
-            Sentence: \"" + review_obj.text + "\"\n\
-            Sentiment:",
+            Sentence: "' + review_obj.text + '"\n\
+            Sentiment:',
         temperature=0,
         max_tokens=64,
         top_p=1,
@@ -46,15 +46,15 @@ def popup(result_text):
     window.close()
 
 def get_product_price():
-    product_price_xpath = """/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[3]/div/div[3]/div/div"""
+    product_price_xpath = "/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[3]/div/div[3]/div/div"
     product_price = driver.find_element(By.XPATH, product_price_xpath).text
     #print("1 "+product_price)
     return product_price
 
 def get_product_info():
-    star_rating_xpath = """/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div[1]"""
-    total_ratings_xpath = """/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[2]/div[1]"""
-    sold_xpath = """/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[3]/div[1]"""
+    star_rating_xpath = "/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div[1]"
+    total_ratings_xpath = "/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[2]/div[1]"
+    sold_xpath = "/html/body/div[1]/div/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[3]/div[1]"
 
     star_rating = driver.find_element(By.XPATH, star_rating_xpath).text
     total_ratings = driver.find_element(By.XPATH, total_ratings_xpath).text
@@ -69,14 +69,14 @@ driver.get('https://shopee.com.my/autobotic/867583382')
 
 WebDriverWait(driver, 6)
 print('Click')
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, """//*[@id="modal"]/div[1]/div[1]/div/div[3]/div[1]/button"""))).click()
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id=\"modal\"]/div[1]/div[1]/div/div[3]/div[1]/button"))).click()
 driver.execute_script("window.scrollTo(0, 2080)")
 
 result_text = "Result:\n"
 print('start scrapping')
 
 for i in range(1, 4):
-    review_xpath = """//*[@id="main"]/div/div[2]/div[2]/div/div[3]/div[2]/div[1]/div[2]/div/div[3]/div[1]/div["""+str(i)+"""]/div/div[@class = 'shopee-product-rating__content']"""
+    review_xpath = "//*[@id=\"main\"]/div/div[2]/div[2]/div/div[3]/div[2]/div[1]/div[2]/div/div[3]/div[1]/div["+str(i)+"]/div/div[@class = 'shopee-product-rating__content']"
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, review_xpath)))
     review_obj = driver.find_element(By.XPATH, review_xpath)
@@ -92,7 +92,7 @@ result_text += get_product_info()
 print(result_text)
 popup(result_text)
 
-next_buttton = driver.find_element(By.XPATH, """//*[@id="main"]/div/div[2]/div[2]/div/div[3]/div[2]/div[1]/div[2]/div/div[3]/div[2]/button[@class = 'shopee-button-solid shopee-button-solid--primary ']/following-sibling::button""")
+next_buttton = driver.find_element(By.XPATH, "//*[@id=\"main\"]/div/div[2]/div[2]/div/div[3]/div[2]/div[1]/div[2]/div/div[3]/div[2]/button[@class = 'shopee-button-solid shopee-button-solid--primary ']/following-sibling::button")
 
 if next_buttton == '':
     print('no more reviews')
